e9e68b0266f1b7b6464ab16955b029b7ac038382
messat <148489126+messat@users.noreply.github.com>
2024-09-20 12:04:29 +0100

    Added new endpoint to get all owners data

--- controller/controller.js
+++ controller/controller.js
@@ -1,13 +1,23 @@
-const { selectOwnersById } = require("../models/models")
+const { selectOwnersById, selectAllOwners } = require("../models/models");
 
+exports.getOwnersById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const ownersFileData = await selectOwnersById(id);
+    res.status(200).send({ "Owner Profile": ownersFileData });
+  } catch (e) {
+    res.status(404).send({ message: "404 Not Found" });
+  }
+};
 
-exports.getOwnersById = async (req, res)=>{
-    const {id} = req.params
-    try {
-        const ownersFileData = await selectOwnersById(id)
-        res.status(200).send({"Owner Profile": ownersFileData})
-    } catch (e){
-        res.status(404).send({message: "404 Not Found"})
+exports.getAllOwners = async (req, res) => {
+  try {
+    const allOwnersData = await selectAllOwners();
+    if (allOwnersData.length){
+        res.status(200).send({ Owners: allOwnersData });
     }
-
-}
+  } catch (e) {
+    console.log(e);
+    res.status(404).send({error: e})
+  }
+};
--- models/models.js
+++ models/models.js
@@ -1,13 +1,30 @@
-const fs = require('fs/promises')
+const fs = require("fs/promises");
 
-exports.selectOwnersById = async (ownersId) =>{
-    try {
-        const ownersProfile = await fs.readFile(`./data/owners/o${ownersId}.json`, 'utf-8')
-        console.log(ownersProfile)
-        const parsedOwnersData = JSON.parse(ownersProfile)
-        return parsedOwnersData
-    } catch (e){
-        throw e
+exports.selectOwnersById = async (ownersId) => {
+  try {
+    const ownersProfile = await fs.readFile(
+      `./data/owners/o${ownersId}.json`,
+      "utf-8"
+    );
+    const parsedOwnersData = JSON.parse(ownersProfile);
+    return parsedOwnersData;
+  } catch (e) {
+    throw e;
+  }
+};
+
+exports.selectAllOwners = async () => {
+  try {
+    const ownersDir = await fs.readdir("./data/owners", "utf-8");
+    const allOwners = [];
+    for (const ownerProfile of ownersDir) {
+      const ownerIdObj = JSON.parse(
+        await fs.readFile(`./data/owners/${ownerProfile}`, "utf-8")
+      );
+      allOwners.push(ownerIdObj);
     }
-
-}
+    return allOwners;
+  } catch (e) {
+    throw e;
+  }
+};
