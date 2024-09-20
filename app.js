46df0cb783db51b47dddc6ba0384a7f2f2d48f78
messat <148489126+messat@users.noreply.github.com>
2024-09-20 11:46:06 +0100

    Added new endpoint and MVC architecture filing system and error handling for owners API Endpoint

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const { getOwnersById } = require('./controller/controller')
 const app = express()
 
 app.get('/api', (req, res)=>{
     res.send("Hello world from the Home page")
 })
+
+app.get('/api/owners/:id', getOwnersById)
 
 app.listen(4040, ()=>{
     console.log("Listening from port 4040")
